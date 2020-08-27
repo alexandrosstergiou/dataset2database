0658a55c36a86411b18cbc10c59dc223223d4c56
Alex Stergiou <astergiou@Alexs-MacBook-Pro.local>
2020-08-27 16:38:58 +0200

    update 27 August 2020 - setup.py changes

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
 setup(name='dataset2database',
-      version='1.0',
+      version='1.1',
       description='Script for creating SQL files from video files',
       url='https://github.com/alexandrosstergiou/dataset2database',
       author='Alexandros Stergiou',
       author_email='alexstergiou5@gmail.com',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       license='MIT',
       packages=['dataset2database'],
       zip_safe=False)
